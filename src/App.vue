1f37786c6834a9da1614cc49207acaab7a202d94
ll <1096195574@qq.com>
2019-09-04 18:34:53 +0800

    one

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    App
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  /*
+  三种css高级编译
+  less sass stylus
+  */
+  /*
+  stylus:
+  结构化：
+  完全通过缩进控制，不需要大括号和分号，冒号可选
+  */
+</style>
---
+++ src/pages/Order/Order.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Order</div>
+</template>
+
+<script>
+  export default {
+    name: 'Order'
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
